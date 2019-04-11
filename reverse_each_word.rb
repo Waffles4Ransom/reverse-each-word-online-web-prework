014d37abe7d78b9c69a7d60dcfabd5ee8e09b83f
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-11 06:05:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   return reverse_ar.join(" ")
 end 
+
+def reverse_each_word(string)
+  new_ar = string.split
+  reverse_ar = [ ]
+  new_ar.each do |string|
+    reverse_ar << string.reverse
+  end
+  return reverse_ar.join(" ")
+end 
